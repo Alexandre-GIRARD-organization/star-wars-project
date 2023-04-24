2460f3c7768be6cabd4f8b630162e5c813f48104
Alexandre GIRARD <alex1dre.girard@gmail.com>
2023-04-24 19:18:57 +0000

    ✨ eslint

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,56 @@
 <script setup>
-import FilmItem from './components/FilmItem.vue';
+import FilmItem from "./components/FilmItem.vue";
 
 const films = [
   {
     episode_id: 4,
-    title: 'A New Hope',
-    director: 'George Lucas',
-    release_date: '1977-05-25'
+    title: "A New Hope",
+    director: "George Lucas",
+    release_date: "1977-05-25",
   },
   {
     episode_id: 5,
-    title: 'The Empire Strikes Back',
-    director: 'Irvin Kershner',
-    release_date: '1980-05-17'
+    title: "The Empire Strikes Back",
+    director: "Irvin Kershner",
+    release_date: "1980-05-17",
   },
   {
     episode_id: 6,
-    title: 'Return of the Jedi',
-    director: 'Richard Marquand',
-    release_date: '1983-05-25'
+    title: "Return of the Jedi",
+    director: "Richard Marquand",
+    release_date: "1983-05-25",
   },
   {
     episode_id: 1,
-    title: 'The Phantom Menace',
-    director: 'George Lucas',
-    release_date: '1999-05-19'
+    title: "The Phantom Menace",
+    director: "George Lucas",
+    release_date: "1999-05-19",
   },
   {
     episode_id: 2,
-    title: 'Attack of the Clones',
-    director: 'George Lucas',
-    release_date: '2002-05-16'
+    title: "Attack of the Clones",
+    director: "George Lucas",
+    release_date: "2002-05-16",
   },
   {
     episode_id: 3,
-    title: 'Revenge of the Sith',
-    director: 'George Lucas',
-    release_date: '2005-05-19'
+    title: "Revenge of the Sith",
+    director: "George Lucas",
+    release_date: "2005-05-19",
   },
-]
+];
 </script>
 
 <template>
   <main>
-    <h1 id="liste_des_films_star_wars">Liste des films star wars:</h1>
-    <ul>
-      <template v-for="film in films">
-        <li>
-          <FilmItem :film="film" />
-        </li>
-      </template>
+    <h1>Liste des films star wars:</h1>
+    <ul id="liste_des_films_star_wars">
+      <li
+        v-for="film in films"
+        :key="film.episode_id"
+      >
+        <FilmItem :film="film" />
+      </li>
     </ul>
-
   </main>
 </template>
--- src/components/FilmItem.vue
+++ src/components/FilmItem.vue
@@ -1,11 +1,15 @@
 <script>
 export default {
     props: {
-        film: Object
+        film: {
+            type: Object,
+            default: () => ({ 
+            })
+        }
     }
 }
 </script>
 
 <template>
-    <strong>{{ film.title }}</strong>, épisode {{ film.episode_id }}, <em>{{ film.release_date }}</em>, {{ film.director }}
+  <strong>{{ film.title }}</strong>, épisode {{ film.episode_id }}, <em>{{ film.release_date }}</em>, {{ film.director }}
 </template>
